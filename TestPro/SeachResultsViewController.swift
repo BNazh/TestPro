6e52d9e98b9eafb86b55b5aa3641ec63e3c625c1
Nazhmeddin <nazhmeddin@auto-nomad.kz>
2020-08-22 00:37:23 +0600

    first commit

---
+++ TestPro/SeachResultsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SeachResultsViewController.swift
+//  TestPro
+//
+//  Created by Nazhmeddin Babakhanov on 21.08.2020.
+//  Copyright © 2020 Nazhmeddin Babakhanov. All rights reserved.
+//
+
+import Foundation
