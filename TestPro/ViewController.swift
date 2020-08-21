241bd7fbea593989526563db2baefe5b3741a199
Nazhmeddin <nazhmeddin@auto-nomad.kz>
2020-08-21 22:16:46 +0600

    Initial Commit

---
+++ TestPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPro
+//
+//  Created by Nazhmeddin Babakhanov on 21.08.2020.
+//  Copyright © 2020 Nazhmeddin Babakhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
